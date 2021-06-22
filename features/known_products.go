ccd35dec1bfe80db4579df294560c90629c4ed35
Pete Wall <pwall@vmware.com>
2021-06-22 13:52:33 -0500

    Update versions in enemy tests

--- features/known_products.go
+++ features/known_products.go
@@ -5,6 +5,6 @@ const (
 	ChartProductVersion = "0.2.4"
 
 	ContainerProductSlug            = "test-container-product-21"
-	ContainerProductVersion         = "0.2.3"
-	ContainerProductVersionButOlder = "0.2.2"
+	ContainerProductVersion         = "0.2.4"
+	ContainerProductVersionButOlder = "0.2.3"
 )
