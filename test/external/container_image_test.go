04742cc465d4f2d46636942c2da052f338df9b63
Pete Wall <pwall@vmware.com>
2022-04-25 09:05:53 -0500

    Fix test
    Signed-off-by: Pete Wall <pwall@vmware.com>

--- test/external/container_image_test.go
+++ test/external/container_image_test.go
@@ -12,11 +12,16 @@ import (
 )
 
 var _ = Describe("Container Image", func() {
+	const (
+		ProductSlug    = "cloudian-s3-compatible-object-storage-for-tkgs0-1-1"
+		ProductVersion = "1.2.1"
+	)
+
 	steps := NewSteps()
 
 	Scenario("Listing container images", func() {
 		steps.Given("targeting the production environment")
-		steps.When("running mkpcli container-image list --product redis-enterprise-kubernetes-operator-for-vmware-enterprise-pks --product-version 5.4.2-27")
+		steps.When("running mkpcli container-image list --product " + ProductSlug + " --product-version " + ProductVersion)
 		steps.Then("the command exits without error")
 		steps.And("the table of container images is printed")
 	})
@@ -28,7 +33,7 @@ var _ = Describe("Container Image", func() {
 			Eventually(CommandSession.Out).Should(Say("IMAGE"))
 			Eventually(CommandSession.Out).Should(Say("TAGS"))
 			Eventually(CommandSession.Out).Should(Say("DOWNLOADS"))
-			Eventually(CommandSession.Out).Should(Say("https://hub.docker.com/u/redislabs"))
+			Eventually(CommandSession.Out).Should(Say("quay.io/cloudian/hyperstore-operator"))
 		})
 	})
 })
